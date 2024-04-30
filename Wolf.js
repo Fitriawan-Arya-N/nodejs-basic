7c01276b89e10df58f2614717ef61ac68ab9183e
Fitriawan-Arya-N <nugrahaaryaa17@gmail.com>
2024-04-30 18:07:41 +0700

    2

---
+++ Wolf.js
@@ -0,0 +1,14 @@
+//Wolf.js
+
+
+class Wolf {
+    constructor() {
+      this.strength = Math.floor(Math.random() * 100);
+    }
+   
+    howl() {
+      console.log('owooooo!')
+    }
+  }
+   
+  module.exports = Wolf;
